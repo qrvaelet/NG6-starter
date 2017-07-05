e620c61e3f288f1718a80b5dd86ffb9d09201e79
Sziszi <sziszi-gitgud@nym.hush.com>
2017-07-06 00:06:03 +0200

    modify the module tree, minor changes

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,9 +1,15 @@
 import angular from 'angular';
 import Home from './home/home';
+import New from './new/new';
+import Search from './search/search';
+import Browse from './browse/browse';
 import About from './about/about';
 
 let componentModule = angular.module('app.components', [
   Home,
+  Browse,
+  Search,
+  New,
   About
 ])
 
